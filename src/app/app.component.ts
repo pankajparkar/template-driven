6dc75f8262ca6905402378759d002b79afc2faf6
Pankaj Parkar <parkarpankaj3@gmail.com>
2025-07-25 15:52:55 +0800

    feat: angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'td-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'td-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'template-driven';
